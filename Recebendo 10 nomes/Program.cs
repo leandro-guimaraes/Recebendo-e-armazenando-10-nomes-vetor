9092c1d6c04d45d0570aacaaa193ee17a7e6e54e
Guest 01 <guest01@alunos.sc.senac.br>
2022-05-09 19:37:02 -0300

    Nomes

---
+++ Recebendo 10 nomes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Recebendo_10_nomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            string[] nomes = new string[10];
+
+            for( int i = 0;i < 10;i++ )
+            {
+                Console.WriteLine("Digite um nome:  ");
+                nomes[i] = Console.ReadLine();
+                Console.Clear();
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Nome armazenado  "+ nomes[i]);
+            }
+
+
+        }
+    }
+}
+
+
